e140bfc4f39e7d908a18145bd054602cf70224a5
qayjjj <trngquanh@gmail.com>
2023-10-16 19:27:53 -0500

    Add demo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Change position to middle
+    <div className="App h-screen grid place-items-center">
+      {/* Add rounded corner, padding */}
+      <div className="w-1/2 bg-white rounded-lg p-6">
+        {/* Make avatar circle, change position to middle */}
+        <img className="h-16 w-16 rounded-full mx-auto" src="cat.png" />
+        {/* Add spacing between avatar and info */}
+        <div className="text-center mt-4">
+          {/* Bold name */}
+          <h2 className="text-lg font-bold">Googoo Gaga</h2>
+          <div className="text-purple-500">Professional Milk Drinker</div>
+          <div className="text-gray-600">googoogaga@milkies.com</div>
+          <div className="text-sm text-gray-600">(123) 123-1234</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
